0c66e1d2f3aeb70ed313654dc1300383e6525e8f
“Alan” <vazquezalanmaximiliano@gmail.com>
2022-06-29 21:58:28 -0300

    first commit

---
+++ js/videolist/script-videolist-platinumend.js
@@ -0,0 +1,57 @@
+let allVideos = [
+    {
+       name: "Episodio 01",
+       src: "../../../img/home/platinumEnd/video.mp4",
+       id: "vid_1"
+    },
+    {
+       name: "Episodio 02",
+       src: "../../../img/home/platinumEnd/video.mp4",
+       id: "vid_2"
+    },
+    {
+       name: "Episodio 03",
+       src: "../../../img/home/platinumEnd/video.mp4",
+       id: "vid_3"
+    },
+    {
+       name: "Episodio 04",
+       src: "../../../img/home/platinumEnd/video.mp4",
+       id: "vid_4"
+    },
+    {
+       name: "Episodio 05",
+       src: "../../../img/home/platinumEnd/video.mp4",
+       id: "vid_5"
+    },
+    {
+       name: "Episodio 06",
+       src: "../../../img/home/platinumEnd/video.mp4",
+       id: "vid_6"
+    },
+    {
+       name: "Episodio 07",
+       src: "../../../img/home/platinumEnd/video.mp4",
+       id: "vid_7"
+    },
+    {
+       name: "Episodio 08",
+       src: "../../../img/home/platinumEnd/video.mp4",
+       id: "vid_8"
+    },
+    {
+       name: "Episodio 09",
+       src: "../../../img/home/platinumEnd/video.mp4",
+       id: "vid_9"
+    },
+    {
+       name: "Episodio 10",
+       src: "../../../img/home/platinumEnd/video.mp4",
+       id: "vid_10"
+    },
+    {
+       name: "Episodio 11",
+       src: "../../../img/home/platinumEnd/video.mp4",
+       id: "vid_11"
+    }
+ ]
